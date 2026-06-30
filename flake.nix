9c05020a8670d2b218494ffbfd86b31b02498d35
hytx <up201906931@up.pt>
2026-06-30 11:38:25 +0100

    Use ellipsis in flake output pattern to pass self

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { nixpkgs }:
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
